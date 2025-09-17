c2c499d4159ef445810b3115055d9b0d5ca1c6c4
c0re100 <corehusky@gmail.com>
2025-09-17 14:33:09 +0800

    Check console is show or not

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,11 @@ namespace GardenHook
     {
         public override void Load()
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            if (Console.LargestWindowWidth > 0)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+            }
+
             Global.Log = Log;
 
             Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
